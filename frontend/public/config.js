acabf3ace1d2a62b46d9a785d4a164f1f8d28b6e
SanduDS <sandaruwandanushka@gmail.com>
2025-07-27 02:18:26 +0530

    update config

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,7 +1,7 @@
 // Runtime configuration for the frontend application
 // This file can be mounted/replaced at deployment time to configure the application
 window.APP_CONFIG = {
-  API_URL: 'http://localhost:8000',
+  API_URL: 'https://30ea7c26-925d-4678-a19d-cf800a59a6bd-dev.e1-us-east-azure.choreoapis.dev/default/bankstatmanagerservice/v1.0',
   
   // Other configurable settings can be added here
   APP_NAME: 'Bank Statement Analyzer',
