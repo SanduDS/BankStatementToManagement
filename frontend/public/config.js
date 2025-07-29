1d9189ae151303a5ca0dce44c7164244b4697274
Dhanushka Sandaruwan <sandaruwandanushka@gmail.com>
2025-07-29 23:06:53 +0530

    Update config.js

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,7 +1,7 @@
 // Choreo runtime configuration for the frontend application
 // This file can be mounted/replaced at deployment time to configure the application
 window.configs = {
-  apiUrl: '/choreo-apis/default/bankstatmanagerservice/v1',
+  apiUrl: '/choreo-apis/hellowproject/backend/v1',
   
   // Other configurable settings can be added here
   appName: 'Bank Statement Analyzer',
